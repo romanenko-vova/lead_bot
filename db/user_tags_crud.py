1103754f70c9afb99ff2c170a59a0ae3ea0054e8
Romanenko Vladimir <bonyshd@gmail.com>
2025-10-12 18:08:12 +0300

    пофиксил ошибку crud

--- db/user_tags_crud.py
+++ db/user_tags_crud.py
@@ -3,8 +3,8 @@ from config.config import TAGS
 async def create_user_tag(user_id: int, tag_name: str):
     if tag_name in TAGS:
         async with aiosqlite.connect('lead.db') as conn:
-            await conn.execute('SELECT id FROM tags WHERE name = ?', (tag_name,))
-            tag_id = await conn.fetchone()
+            cur = await conn.execute('SELECT id FROM tags WHERE name = ?', (tag_name,))
+            tag_id = await cur.fetchone()
             await conn.execute('INSERT INTO users_tags (user_id, tag_id) VALUES (?, ?)', (user_id, tag_id[0]))
             await conn.commit()
             return True
